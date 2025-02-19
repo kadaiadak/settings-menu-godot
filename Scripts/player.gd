9d9d72e1eecf3a782b334e611d143c5aec916b30
kadaiadak <teo.le.greato@gmail.com>
2025-02-18 17:19:55 -0800

    init

---
+++ Scripts/player.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+
+func _physics_process(delta: float):
+	var directionx := Input.get_axis("left", "right")
+	if directionx:
+		velocity.x = directionx * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	var directiony := Input.get_axis("up", "down")
+	if directiony:
+		velocity.y = directiony * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
